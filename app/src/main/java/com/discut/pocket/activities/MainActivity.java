99ff9af97703c5b38f18f7acf621cde55846361c
Discut <siiiro@outlook.com>
2022-09-16 10:54:39 +0800

    feat(MainPage):添加首页模板

---
+++ app/src/main/java/com/discut/pocket/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.discut.pocket.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.discut.pocket.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
