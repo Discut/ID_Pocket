c047f67a0e6249405483fa1f01fdecfbc7e0a762
Discut <siiiro@outlook.com>
2022-09-16 19:04:22 +0800

    feat(MainPage):更新MainActivity组件

--- app/src/main/java/com/discut/pocket/activities/MainActivity.java
+++ app/src/main/java/com/discut/pocket/activities/MainActivity.java
@@ -1,10 +1,20 @@
 package com.discut.pocket.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.discut.pocket.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +22,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+
+
+        ((LinearLayout) findViewById(R.id.linear_box)).setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                int top = WindowInsetsCompat.toWindowInsetsCompat(insets, v).getInsets(WindowInsetsCompat.Type.statusBars()).top;
+                v.setPadding(0,top,0,0);
+                return insets;
+            }
+        });
+
+        LinearLayout viewById = (LinearLayout)findViewById(R.id.box_list);
+
+
+
+        for (int i = 0; i < 200; i++){
+            TextView viewT = new TextView(this);
+            viewT.setText("Id+"+i);
+            viewT.setTextSize(30);
+            viewById.addView(viewT);
+        }
+
     }
 }
