261889daa42ca2a3d7ce4cdc07451ee38d4097d9
Discut <siiiro@outlook.com>
2022-09-16 10:53:48 +0800

    feat(BootPage):添加了指纹验证支持，添加了明暗主题切换支持

--- app/src/main/java/com/discut/pocket/activities/BootActivity.java
+++ app/src/main/java/com/discut/pocket/activities/BootActivity.java
@@ -1,20 +1,37 @@
 package com.discut.pocket.activities;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
 import com.discut.pocket.R;
 import com.discut.pocket.utils.BiometricUtil;
 
 public class BootActivity extends AppCompatActivity {
 
+/*    @Override
+    protected void onStart() {
+        super.onStart();
+
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
+    }*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.boot_layout);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+
 
         Button btn = findViewById(R.id.fingerprint_btn);
         BootActivity bootActivity = this;
@@ -29,6 +46,8 @@ public class BootActivity extends AppCompatActivity {
                 @Override
                 public void success() {
                     Log.d("TAG", "success: 验证成功的消息");
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
                 }
 
             });
